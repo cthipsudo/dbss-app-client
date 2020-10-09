4932b8b34ad558772e44dea0c131efa655859115
GrouchyDodaoOT <chanpasong.thipphakhinkeo@gmail.com>
2020-10-09 16:37:42 -0500

    added deploy endpoint

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    API_ENDPOINT: 'http://localhost:8000/api',
+    API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000/api',
     TOKEN_KEY: process.env.REACT_APP_GAME_TOKEN || 'false token',
     PLAYER_DATA: 'game-storage',
     CHAR_DATA: 'current-char',
