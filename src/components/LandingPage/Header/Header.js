d5b075b24f493c7e78c41bd543e3f1b3a9f95a2f
GrouchyDodaoOT <chanpasong.thipphakhinkeo@gmail.com>
2020-10-15 14:17:38 -0500

    jazzed up desktop header

--- src/components/LandingPage/Header/Header.js
+++ src/components/LandingPage/Header/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import SpaceShip from '../../../images/spaceship.svg'
 import './Header.css';
 
 export default class Header extends Component {
@@ -7,6 +8,7 @@ export default class Header extends Component {
             <header className="lpHeader">
                 <h1>Drifting Between Silence and Stardust</h1>
                 <h2>A creative narrative game crafted by Chanpasong Thipphakhinkeo</h2>
+                <img className="mobileHidden" src={SpaceShip} alt="spaceship"></img>
             </header>
         )
     }
