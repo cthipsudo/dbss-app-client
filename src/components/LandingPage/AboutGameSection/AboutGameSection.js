556099bebb34ce5a16924482dcb22c8ead82503d
GrouchyDodaoOT <chanpasong.thipphakhinkeo@gmail.com>
2020-10-16 10:14:48 -0500

    changed features section styles

--- src/components/LandingPage/AboutGameSection/AboutGameSection.js
+++ src/components/LandingPage/AboutGameSection/AboutGameSection.js
@@ -6,11 +6,11 @@ export default class AboutGameSection extends Component {
             <section className="lpSection">
                 <h3>About the Game:</h3>
                 <p className="noMargin">Drifting Between Silence and Startdust is a variant on a "Choose-your-own-Adventure" game where you can create a character and journey across the stars along to overcome a randomly generated situation from a pool of scenarios, if the player can overcome the hardships and make the right choices, their character will be placed in the Hall of Fame among the other successful "pilots".</p>
-                <h4>It Features:</h4>
+                <h4>Features:</h4>
                 <ul>
                     <li>Randomly shuffled encounters after the 1st Tutorial question on each playthrough.</li>
                     <li>Unique responses are generated for each encounter based on your choices.</li>
-                    <li>Choices are catered specifically to a character's race and class.</li>
+                    <li>Character specific choices based on a character's race and class.</li>
                 </ul>
             </section>
         )
