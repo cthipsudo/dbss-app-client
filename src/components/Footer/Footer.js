dae3ece63b7b395f34ff6e3f340dde3499956f31
GrouchyDodaoOT <chanpasong.thipphakhinkeo@gmail.com>
2026-04-01 19:36:23 -0400

    updated footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,17 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './Footer.css'
+import "./Footer.css";
 
 export default class Footer extends Component {
-    render() {
-        return (
-            <footer id="footer">
-                <p>Built by cthipsudo</p>
-                <p className="footerSpacer">|</p>
-                <p>All Rights Reserved.</p>
-                <p className="footerSpacer">|</p>
-                <a href="https://oscarthipp.com/"><p>See my other projects!</p></a>
-            </footer>
-        )
-    }
+  render() {
+    return (
+      <footer id="footer">
+        <p>Built by cthipsudo</p>
+        <p className="footerSpacer">|</p>
+        <p>All Rights Reserved.</p>
+        <p className="footerSpacer">|</p>
+        <a href="https://othipp.dev/">
+          <p>See my other projects!</p>
+        </a>
+      </footer>
+    );
+  }
 }
